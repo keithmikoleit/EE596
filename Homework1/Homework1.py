39a4121113635af0c6185ab6fdc7c5d6da2db522
KeithMikoleit <keith.mikoleit@gmail.com>
2015-10-07 23:36:09 -0700

    Updated Homework1 based on 10.7 feedback
    Updated Homework.py to no longer take user input.  Calls find connectec
    components on 3 base images.
    Updated find connected components to perform close morphology on opened
    image, instead of operations in parallel, to further enhance components
    after they have been eroded.
    Udpated to draw each connectec component in a different color.

--- Homework1/Homework1.py
+++ Homework1/Homework1.py
@@ -6,69 +6,80 @@ Created on Oct 6, 2015
 
 import cv2
 import numpy
+from random import randint
 
 
-def findConnectedComponents(image_name, ellipse_size):
+def findConnectedComponents(image_name, open_ellipse_size, close_ellipse_size):
 
     #get original image and display
-    im_orig = cv2.imread(image_name, 0)
+    im_orig = cv2.imread(image_name, cv2.IMREAD_COLOR)
     
     cv2.imshow('Original', im_orig)
     
-    cv2.waitKey(0)
+    # cv2.waitKey(0)
     
     #threshold the image to create a binary image
     threshold = 128
     maxval = 255
     _, im_th = cv2.threshold(im_orig,threshold, maxval, cv2.THRESH_BINARY)
     
-    cv2.imshow('Threshold', im_th)
+    #convert image to gray scale type
+    im_gray = cv2.cvtColor(im_th, cv2.COLOR_RGB2GRAY)
     
-    cv2.waitKey(0)
+    cv2.imshow('Threshold', im_gray)
     
-    # create a circular mask and use it to morph the image via opening and closing 
-    # dont need to do this since we are using the countours method
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, ellipse_size)
+    # cv2.waitKey(0)
     
-    im_open = cv2.morphologyEx(im_th, cv2.MORPH_OPEN, kernel)
+    # create a circular mask and use it to morph the image via opening
+    kernel_open = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, open_ellipse_size)
+    im_open = cv2.morphologyEx(im_gray, cv2.MORPH_OPEN, kernel_open)
       
-    im_close = cv2.morphologyEx(im_th, cv2.MORPH_CLOSE, kernel) 
+    # create a smaller mask and use it to morph the image via closing to get rid of
+    # small imperfections
+    kernel_close = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, close_ellipse_size)
+    im_close = cv2.morphologyEx(im_open, cv2.MORPH_CLOSE, kernel_close) 
     
     cv2.imshow('Kidney Open', im_open)
     cv2.imshow('Kidney Close', im_close)
     
-    cv2.waitKey(0)
+    # cv2.waitKey(0)
     
     #get the countours 
-    countours,_ = cv2.findContours(im_open, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
+    contours,_ = cv2.findContours(im_close, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     
     img_bin = numpy.zeros((im_orig.shape[0], im_orig.shape[1],3),numpy.uint8)
+
+    #cv2.drawContours(img_bin, countours, -1, (255, 0, 255), -1)
+    for i in range(0,len(contours)):
+        cv2.drawContours(img_bin, contours, i, (randint(0,255),randint(0,255),randint(0,255)), -1)
+        
+    #cv2.drawContours(img_bin, contours, -1, (255, 0, 255), -1)
     
-    cv2.drawContours(img_bin, countours, -1, (255, 0, 255), -1)
+    cv2.imshow(image_name + ": Connected Components", img_bin)
     
-    cv2.imshow("Connected Components", img_bin)
+    cv2.waitKey(0)
     
-    cv2.waitKey(0) 
+    # clear all windows
+    cv2.destroyAllWindows() 
 
 if __name__ == '__main__':
-    image1 = 'E:\UW PMP\EE596\g006.png'
-    image2 = 'E:\UW PMP\EE596\e030.png'
-    image3 = 'E:\UW PMP\EE596\kidney.png'
-    image = ''
-    cmd = ''
-    print('Enter the image file or q to quite')
-    while cmd != 'q':
-        cmd = raw_input('Which image do you want to process? ')
-        if cmd != 'q':
-            ellipse_size = tuple(int(x.strip()) for x in raw_input('Enter mask size (as ellipse parameters in a tuple) >> ').split(','))
-            
-            if cmd == '1':
-                image = image1
-            elif cmd == '2':
-                image = image2
-            elif cmd == '3':
-                image = image3
-            print (image)
-            findConnectedComponents(image, ellipse_size)
+    image1 = 'g006.png'
+    image2 = 'e030.png'
+    image3 = 'kidney.png'
+
+    # find connected components for g006.png
+    open_ellipse_size = (17,17)
+    close_ellipse_size = (1,1)
+    findConnectedComponents(image1, open_ellipse_size, close_ellipse_size)
+    
+    # find connected components for e030.png
+    open_ellipse_size = (30,30)
+    close_ellipse_size = (1,1)
+    findConnectedComponents(image2, open_ellipse_size, close_ellipse_size)
+    
+    # find connected components for kidney.png
+    open_ellipse_size = (17,17)
+    close_ellipse_size = (2,2)
+    findConnectedComponents(image3, open_ellipse_size, close_ellipse_size)
         
     print('Done')
